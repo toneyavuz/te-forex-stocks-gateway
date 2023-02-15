4ea9d59e4c6024d4c847e6f59b1e2e632a81b4fb
toneyavuz <toneyavuz@gmail.com>
2023-02-16 02:55:59 +0300

    add config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './controller/app.controller';
 import { AppService } from './service/app.service';
 import { CalculatorModule } from './module/calculator/calculator.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CalculatorModule],
+  imports: [CalculatorModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
