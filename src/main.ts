71536a6fbcfa33984199b45441ef13e379f51db6
toneyavuz <toneyavuz@gmail.com>
2023-02-16 02:49:34 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Stocks API')
+    .setDescription(
+      'The Stocks API is a simple calculator for foreign exchange market.',
+    )
+    .setVersion('1.0')
+    .addTag('stocks')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
