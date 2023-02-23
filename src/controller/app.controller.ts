0a7bec659e06bbcaa6cc82e62aa6123fac83890a
toneyavuz <toneyavuz@gmail.com>
2023-02-23 22:39:59 +0300

    update docker compose

--- src/controller/app.controller.ts
+++ src/controller/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/module/authentication/decorator/public.decorator';
 import { AppService } from '../service/app.service';
 
 @Controller()
@@ -8,6 +9,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
   getHello(): string {
     return this.appService.getHello();
   }
